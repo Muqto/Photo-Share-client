6b9f0e4cc2de00555b2ae33e41010f4f88ee3ad6
Muqto <muqto14@gmail.com>
2022-12-17 16:42:37 -0500

    changed to render

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const API = axios.create({baseURL: 'https://photo-share-mern-app.herokuapp.com/'})
+const API = axios.create({baseURL: 'https://photo-share-api.onrender.com/'})
 
 API.interceptors.request.use((req) => {
     if(localStorage.getItem('profile')){
